f4ac3fb47bd3619c062a545e024f90f259a93c10
youscmk <thesteam2010@live.com>
2023-01-10 15:12:34 -0300

    todos los campos funcionando con la base de datos

--- listabuses.php
+++ listabuses.php
@@ -2,7 +2,7 @@
 include "login/login.php";
 include "login/conexion.php";
 
-
+header("refresh:2");
 
 $curl = curl_init();
 
@@ -123,11 +123,47 @@ foreach ($array as $item) {
   echo $lng = $response1->state->gps->location->lng . " , &nbsp;";
   echo $last_u = $response1->state->last_update . " , &nbsp";
 
+$latitud = $response1->state->gps->location->lat;
+$longitud = $response1->state->gps->location->lng;
+
+  $curl = curl_init();
+
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => 'http://www.trackermasgps.com/api-v2/geocoder/search_location',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS =>'{"location":{"lat":"'.$latitud.'","lng":"'.$longitud.'"},"lang":"es","hash":"4c995ab8b8dfe16e6f265c26c39561d9"}',
+    CURLOPT_HTTPHEADER => array(
+      'Accept: application/json, text/plain, */*',
+      'Accept-Language: es-419,es;q=0.9,en;q=0.8',
+      'Connection: keep-alive',
+      'Content-Type: application/json',
+      'Cookie: _ga=GA1.2.728367267.1665672802; _gid=GA1.2.343013326.1670594107; locale=es; session_key=7ceae24d013dd694bdbaa06dd8bac781; check_audit=7ceae24d013dd694bdbaa06dd8bac781',
+      'Origin: http://www.trackermasgps.com',
+      'Referer: http://www.trackermasgps.com/',
+      'User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Mobile Safari/537.36'
+    ),
+  ));
+  
+  $response = curl_exec($curl);
+  
+  curl_close($curl);
+  
+$json1 = json_decode($response);
+
+$array1 = $json1->value;
+echo $array1."<br>";
+
   if ($id == 10177116) {
 
     echo $patente1;
 
-    $sql = "INSERT INTO estadisticas(id_tracker,latitud,longitud,last_update,patente) VALUES ('$id', '$lat', '$lng', '$last_u', '$patente1')";
+    $sql = "INSERT INTO estadisticas(id_tracker,latitud,longitud,last_update,patente,direccion_usuario) VALUES ('$id', '$lat', '$lng', '$last_u', '$patente1', '$array1')";
 
 
     $datosduplicados = mysqli_query($mysqli, "SELECT * FROM estadisticas WHERE last_update= '$last_u' ");
@@ -141,7 +177,7 @@ foreach ($array as $item) {
   if ($id == 10177117) {
     echo $patente2;
 
-    $sql = "INSERT INTO estadisticas (id_tracker,latitud,longitud,last_update,patente) VALUES ('$id', '$lat', '$lng', '$last_u', '$patente2')";
+    $sql = "INSERT INTO estadisticas (id_tracker,latitud,longitud,last_update,patente,direccion_usuario) VALUES ('$id', '$lat', '$lng', '$last_u', '$patente2', '$array1')";
 
     $datosduplicados = mysqli_query($mysqli, "SELECT * FROM estadisticas WHERE last_update= '$last_u' ");
 
@@ -154,7 +190,7 @@ foreach ($array as $item) {
   if ($id == 10177118) {
     echo $patente3;
 
-    $sql = "INSERT INTO estadisticas (id_tracker,latitud,longitud,last_update,patente) VALUES ('$id', '$lat', '$lng', '$last_u', '$patente3')";
+    $sql = "INSERT INTO estadisticas (id_tracker,latitud,longitud,last_update,patente,direccion_usuario) VALUES ('$id', '$lat', '$lng', '$last_u', '$patente3','$array1')";
 
     $datosduplicados = mysqli_query($mysqli, "SELECT * FROM estadisticas WHERE last_update= '$last_u' ");
 
@@ -167,7 +203,7 @@ foreach ($array as $item) {
   if ($id == 10177119) {
     echo $patente4;
 
-    $sql = "INSERT INTO estadisticas (id_tracker,latitud,longitud,last_update,patente) VALUES ('$id', '$lat', '$lng', '$last_u', '$patente4')";
+    $sql = "INSERT INTO estadisticas (id_tracker,latitud,longitud,last_update,patente,direccion_usuario) VALUES ('$id', '$lat', '$lng', '$last_u', '$patente4', '$array1')";
 
     $datosduplicados = mysqli_query($mysqli, "SELECT * FROM estadisticas WHERE last_update= '$last_u' ");
 
@@ -180,7 +216,7 @@ foreach ($array as $item) {
   if ($id == 10177120) {
     echo $patente5;
 
-    $sql = "INSERT INTO estadisticas (id_tracker,latitud,longitud,last_update,patente) VALUES ('$id', '$lat', '$lng', '$last_u', '$patente5')";
+    $sql = "INSERT INTO estadisticas (id_tracker,latitud,longitud,last_update,patente,direccion_usuario) VALUES ('$id', '$lat', '$lng', '$last_u', '$patente5', '$array1')";
     $datosduplicados = mysqli_query($mysqli, "SELECT * FROM estadisticas WHERE last_update= '$last_u' ");
 
     if (mysqli_num_rows($datosduplicados) > 0) {
@@ -192,7 +228,7 @@ foreach ($array as $item) {
   if ($id == 10177121) {
     echo $patente6;
 
-    $sql = "INSERT INTO estadisticas (id_tracker,latitud,longitud,last_update,patente) VALUES ('$id', '$lat', '$lng', '$last_u', '$patente6')";
+    $sql = "INSERT INTO estadisticas (id_tracker,latitud,longitud,last_update,patente,direccion_usuario) VALUES ('$id', '$lat', '$lng', '$last_u', '$patente6', '$array1')";
 
     $datosduplicados = mysqli_query($mysqli, "SELECT * FROM estadisticas WHERE last_update= '$last_u' ");
 
@@ -205,7 +241,7 @@ foreach ($array as $item) {
   if ($id == 10177122) {
     echo $patente7;
 
-    $sql = "INSERT INTO estadisticas (id_tracker,latitud,longitud,last_update,patente) VALUES ('$id', '$lat', '$lng', '$last_u', '$patente7')";
+    $sql = "INSERT INTO estadisticas (id_tracker,latitud,longitud,last_update,patente,direccion_usuario) VALUES ('$id', '$lat', '$lng', '$last_u', '$patente7', '$array1')";
     $datosduplicados = mysqli_query($mysqli, "SELECT * FROM estadisticas WHERE last_update= '$last_u' ");
 
     if (mysqli_num_rows($datosduplicados) > 0) {
@@ -217,7 +253,7 @@ foreach ($array as $item) {
   if ($id == 10177123) {
     echo $patente8;
 
-    $sql = "INSERT INTO estadisticas (id_tracker,latitud,longitud,last_update,patente) VALUES ('$id', '$lat', '$lng', '$last_u', '$patente8')";
+    $sql = "INSERT INTO estadisticas (id_tracker,latitud,longitud,last_update,patente,direccion_usuario) VALUES ('$id', '$lat', '$lng', '$last_u', '$patente8','$array1')";
     $datosduplicados = mysqli_query($mysqli, "SELECT * FROM estadisticas WHERE last_update= '$last_u' ");
 
     if (mysqli_num_rows($datosduplicados) > 0) {
@@ -229,7 +265,7 @@ foreach ($array as $item) {
   if ($id == 10177124) {
     echo $patente9;
 
-    $sql = "INSERT INTO estadisticas (id_tracker,latitud,longitud,last_update,patente) VALUES ('$id', '$lat', '$lng', '$last_u', '$patente9')";
+    $sql = "INSERT INTO estadisticas (id_tracker,latitud,longitud,last_update,patente,direccion_usuario) VALUES ('$id', '$lat', '$lng', '$last_u', '$patente9','$array1')";
     $datosduplicados = mysqli_query($mysqli, "SELECT * FROM estadisticas WHERE last_update= '$last_u' ");
 
     if (mysqli_num_rows($datosduplicados) > 0) {
@@ -241,7 +277,7 @@ foreach ($array as $item) {
   if ($id == 10177125) {
     echo $patente10;
 
-    $sql = "INSERT INTO estadisticas (id_tracker,latitud,longitud,last_update,patente) VALUES ('$id', '$lat', '$lng', '$last_u', '$patente10')";
+    $sql = "INSERT INTO estadisticas (id_tracker,latitud,longitud,last_update,patente,direccion_usuario) VALUES ('$id', '$lat', '$lng', '$last_u', '$patente10','$array1')";
     $datosduplicados = mysqli_query($mysqli, "SELECT * FROM estadisticas WHERE last_update= '$last_u' ");
 
     if (mysqli_num_rows($datosduplicados) > 0) {
