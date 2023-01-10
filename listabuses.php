2951df1270d892d6ca703a8a5560db02e4327765
youscmk <thesteam2010@live.com>
2023-01-10 10:15:24 -0300

    first commit

---
+++ listabuses.php
@@ -0,0 +1,109 @@
+<?php
+include "login/login.php";
+
+
+
+$lruc = curl_init();
+
+curl_setopt_array($lruc, array(
+  CURLOPT_URL => 'http://www.trackermasgps.com/api-v2/tracker/list',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS =>'{"hash":"'.$cap.'"}',
+  CURLOPT_HTTPHEADER => array(
+    'Accept: application/json, text/plain, */*',
+    'Accept-Language: es-419,es;q=0.9,en;q=0.8',
+    'Connection: keep-alive',
+    'Content-Type: application/json',
+    'Cookie: _ga=GA1.2.728367267.1665672802; locale=es; _gid=GA1.2.967319985.1673009696; _gat=1; session_key=5d7875e2bf96b5966225688ddea8f098',
+    'Origin: http://www.trackermasgps.com',
+    'Referer: http://www.trackermasgps.com/',
+    'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+  ),
+));
+
+$response = curl_exec($lruc);
+
+curl_close($lruc);
+
+$json=json_decode($response);
+
+$array=$json->list;
+
+foreach ($array as $item){
+echo "<br>";
+  echo $id= $item->id;
+
+
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'http://www.trackermasgps.com/api-v2/tracker/get_state',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS =>'{"hash": "'.$cap.'", "tracker_id": '.$id.'}',
+  CURLOPT_HTTPHEADER => array(
+    'Content-Type: application/json'
+  ),
+));
+
+$response1 = curl_exec($curl);
+
+$response1= json_decode($response1);
+
+curl_close($curl);
+
+//$.state.gps.location.lat
+
+echo $response1->state->gps->location->lat." , &nbsp;";
+echo $lng= $response1->state->gps->location->lng." , &nbsp;";
+echo $response1->state->last_update." , &nbsp";
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'http://www.trackermasgps.com/api-v2/tracker/list',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS =>'{"hash":"'.$cap.'"}',
+  CURLOPT_HTTPHEADER => array(
+    'Accept: application/json, text/plain, */*',
+    'Accept-Language: es-419,es;q=0.9,en;q=0.8',
+    'Connection: keep-alive',
+    'Content-Type: application/json',
+    'Cookie: _ga=GA1.2.728367267.1665672802; locale=es; _gid=GA1.2.967319985.1673009696; _gat=1; session_key=5d7875e2bf96b5966225688ddea8f098',
+    'Origin: http://www.trackermasgps.com',
+    'Referer: http://www.trackermasgps.com/',
+    'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+  ),
+));
+
+$response2 = curl_exec($curl);
+$response2= json_decode($response2);
+
+curl_close($curl);
+echo $response2->list[1]->label;
+
+
+
+}
+
+
+
+?>
