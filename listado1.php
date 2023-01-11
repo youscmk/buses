6c806fa93dbc6a25c856c49c5fb31ee569672278
youscmk <thesteam2010@live.com>
2023-01-11 14:41:58 -0300

    commit funcionando sin datos duplicados

--- listado1.php
+++ listado1.php
@@ -1,5 +1,6 @@
 <?php
 include "login/login.php";
+include "login/conexion.php";
 
 
 //header("refresh:2");
@@ -30,55 +31,95 @@ curl_setopt_array($curl, array(
 
 $response2 = curl_exec($curl);
 
-$json= json_decode($response2);
+$json = json_decode($response2);
 
-$array=$json->list;
+$array = $json->list;
 
-foreach ($array as $item){
+foreach ($array as $item) {
 
-    echo "<br>";
-    echo $id=$item->id;
-    echo "<br>";
-    echo $plate=$item->label;
+  echo "<br>";
+  echo $id = $item->id;
+  echo " , &nbsp";
 
 
-    $curl = curl_init();
-    
-    curl_setopt_array($curl, array(
-      CURLOPT_URL => 'http://www.trackermasgps.com/api-v2/tracker/get_state',
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => '',
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 0,
-      CURLOPT_FOLLOWLOCATION => true,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => 'POST',
-      CURLOPT_POSTFIELDS =>'{"hash": "'.$cap.'", "tracker_id": '.$id.'}',
-      CURLOPT_HTTPHEADER => array(
-        'Content-Type: application/json'
-      ),
-    ));
-    
-    $response2 = curl_exec($curl);
-    
-    curl_close($curl);
+  $curl = curl_init();
 
-    $json2=json_decode($response2);
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => 'http://www.trackermasgps.com/api-v2/tracker/get_state',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS => '{"hash": "' . $cap . '", "tracker_id": ' . $id . '}',
+    CURLOPT_HTTPHEADER => array(
+      'Content-Type: application/json'
+    ),
+  ));
 
-    //$.state.gps.location.lat
 
-    echo "<br>";
+  $response2 = curl_exec($curl);
 
-   echo $lat= $array2=$json2->state->gps->location->lat ;
+  curl_close($curl);
 
-   echo "<br>";
+  $json2 = json_decode($response2);
+  //$.state.gps.location.lat
+  echo " , &nbsp";
 
-   echo $lng= $array2=$json2->state->gps->location->lng ;
+  echo $lat = $array2 = $json2->state->gps->location->lat;
+  echo " , &nbsp";
 
-   echo "<br>";
+  echo $lng = $array2 = $json2->state->gps->location->lng;
+  echo " , &nbsp";
 
-   echo $lng= $array2=$json2->state->last_update ;
-    
+  echo $last_u = $array2 = $json2->state->last_update;
+  echo " , &nbsp";
 
-  
+  echo $plate = $item->label;
+  echo " , &nbsp";
+
+
+  $curl = curl_init();
+
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => 'http://www.trackermasgps.com/api-v2/geocoder/search_location',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS => '{"location":{"lat":' . $lat . ',"lng":' . $lng . '},"lang":"es","hash":"' . $cap . '"}',
+    CURLOPT_HTTPHEADER => array(
+      'Accept: application/json, text/plain, */*',
+      'Accept-Language: es-419,es;q=0.9,en;q=0.8',
+      'Connection: keep-alive',
+      'Content-Type: application/json',
+      'Cookie: _ga=GA1.2.728367267.1665672802; _gid=GA1.2.343013326.1670594107; locale=es; session_key=7ceae24d013dd694bdbaa06dd8bac781; check_audit=7ceae24d013dd694bdbaa06dd8bac781',
+      'Origin: http://www.trackermasgps.com',
+      'Referer: http://www.trackermasgps.com/',
+      'User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Mobile Safari/537.36'
+    ),
+  ));
+
+  $response = curl_exec($curl);
+
+  curl_close($curl);
+  $json1 = json_decode($response);
+  $array4 = $json1->value;
+  echo $array4;
+
+  $sql = "INSERT INTO LPF (id_tracker,latitud,longitud,last_update,patente,direccion_usuario) VALUES ('$id', '$lat', '$lng', '$last_u', '$plate', '$array4')";
+
+  $datosduplicados = mysqli_query($mysqli, "SELECT * FROM LPF WHERE last_update= '$last_u' ");
+
+
+
+  if (mysqli_num_rows($datosduplicados) > 0) {
+  } else {
+    $ejecutar = mysqli_query($mysqli, $sql);
+  }
 }
