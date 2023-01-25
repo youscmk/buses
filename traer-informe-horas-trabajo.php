94a5fb6d225879762acc2c695bcf339ee081dc49
youscmk <thesteam2010@live.com>
2023-01-25 15:59:26 -0300

    filtro ralenti funcionando

--- traer-informe-horas-trabajo.php
+++ traer-informe-horas-trabajo.php
@@ -152,6 +152,7 @@ foreach ($buses as $items) {
   $VarPatente= $divpatente[0];
   $VarId= $divpatente[1];
 
+
   $sql = "INSERT INTO reporte_ralenti (patenteV,patenteId, total_horas, ralenti, en_movimiento, fecha) VALUES ('$plate','$VarId', '$total_horas', '$ralenti', '$en_movimiento', '$fecha_ayer')";
 
   $datosduplicados = mysqli_query($mysqli, "SELECT * FROM reporte_ralenti WHERE fecha='$fecha_ayer' AND patenteId='$VarId' OR total_horas='$total_horas' OR ralenti='$ralenti' OR en_movimiento='$en_movimiento'");
