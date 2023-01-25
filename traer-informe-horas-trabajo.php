beccc1d913418a10ca663bbaf8ee5c47d11a3bd6
youscmk <thesteam2010@live.com>
2023-01-25 10:47:51 -0300

    c

--- traer-informe-horas-trabajo.php
+++ traer-informe-horas-trabajo.php
@@ -149,7 +149,7 @@ foreach ($buses as $items) {
 
   $sql = "INSERT INTO reporte_ralenti (patente, total_horas, ralenti, en_movimiento, fecha) VALUES ('$plate', '$total_horas', '$ralenti', '$en_movimiento', '$fecha_ayer')";
   
-  $datosduplicados = mysqli_query($mysqli, "SELECT * FROM reporte_velocidad WHERE total_horas='$total_horas' AND ralenti='$ralenti' AND en_movimiento='$en_movimiento' AND fecha_ayer='$fecha_ayer'");
+  $datosduplicados = mysqli_query($mysqli, "SELECT * FROM reporte_ralenti WHERE total_horas='$total_horas' AND ralenti='$ralenti' AND en_movimiento='$en_movimiento' AND fecha_ayer='$fecha_ayer'");
   if (mysqli_num_rows($datosduplicados) > 0) {
   } else {
 
