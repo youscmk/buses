fa92fd52f5c26b1f90131f7f51fb5b22033c8c51
Dorian <dorian.celu@gmail.com>
2023-03-09 16:27:27 -0300

    telefono

---
+++ llamada.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+  
+
+<form action="/action_page.php">
+  <label for="phone">llamar a:</label><br><br>
+  <input type="tel" value="950906625" required><br><br>
+ 
+  <input type="submit">
+</form>
+
+</body>
+</html>
