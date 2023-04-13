a1a4b50f0d52bcbfab9c24dda7851974d47bc665
Dorian <dorian.celu@gmail.com>
2023-04-13 09:14:05 -0400

    maqueta

--- llamada.php
+++ llamada.php
@@ -4,20 +4,111 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+    <title>sporlife</title>
+    <style>
+        *{
+            margin-top: 0px;
+            margin-bottom: 0px;
+            padding-left: 0%;
+            padding-right: 0%;
+        }
+        .container {
+            font-family: sans-serif;
+            max-width: 375px;
+            height: 812px;
+            position: relative;
+            margin: auto;
+            color: #0377FF;
 
-    <title>Document</title>
+        }
+        #logo {
+           
+            height: 47.98px;
+            width: 114.63px;
+           
+            position: absolute;
+            margin-top: 46.31px;
+            margin-left: 130px;
+            
+
+        }
+        #help {
+          
+           
+            height: 54px;
+            width:190px ;
+            position: absolute;
+            margin-top: 185px;
+            margin-left: 98px;
+            text-align: center;
+        }
+        p#titulo {
+            
+            font-size: 20px;
+            font-weight: bold;
+
+        }
+
+        p {
+            
+            font-size: 12px;
+        }
+
+        #telefono {
+            position: absolute;
+            margin-top:304px ;
+            margin-left: 137px;
+        }
+
+        #horario {
+           position: absolute;
+           width: 206px;
+           height: 54px; 
+           margin-top: 482px;
+           margin-left: 85px;
+           
+          
+           
+           
+
+        }
+        #horario p {
+            max-width: 206px;
+            font-size: 16px;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
+
+<div class="container">
+
+   
+
+    <div id="logo">
+        <img  src="./img/Grupo 9.svg" alt="sportlife">
+    </div>
+    <div id="help">
+
+        <p id="titulo">¿Necesitas ayuda?</p>
+        <p>Toca boton LLAMAR, para</p>
+        <p>Contatar a un ejecutivo</p>
+
+    </div>
+    <div id="telefono">
+
+       <a href="tel:56989526365"> <img src="./img/Grupo 8.svg" width="100.35px" alt=""></a>
+
+    </div>
+
+    <div id="horario">
+        <p>Horario de atención :</p>
+        <p>Lunes a Domingo de</p>
+        <p>09:00 a 18:00 hrs</p>
+    </div>
+
     
-  <div class="container">
-  <a href="tel:+56989526365" class="btn btn-primary "  >llamar</a>
-  </div>
-
-
- 
-
-
-
+</div>
+    
 </body>
 </html>
