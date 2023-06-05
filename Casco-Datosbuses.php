8a802fe7f07129177bf6a09dcb41bbbf351c2fa0
Dorian <dorian.celu@gmail.com>
2023-06-05 11:03:44 -0400

    datos de buses desde el casco amarillo

---
+++ Casco-Datosbuses.php
@@ -0,0 +1,35 @@
+<?php
+
+
+$pat=$_GET['pat'];
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://api.pullman.cl/srv-vehiculo-core-web/rest/integracionWit/obtenerInformacionVehiculo',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS =>'
+{
+    "patente": "'.$pat.'"
+}
+',
+  CURLOPT_HTTPHEADER => array(
+    'Content-Type: application/json'
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
+
+
+
+
+?>
