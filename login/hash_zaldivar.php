ca3d7c19a64ffa8ea38e5ee32e8ddf52c65ede08
Dorian <dorian.celu@gmail.com>
2023-07-25 17:27:30 -0400

    hora Zaldivar update

--- login/hash_zaldivar.php
+++ login/hash_zaldivar.php
@@ -4,13 +4,18 @@ include "login-zaldivar.php";
 
 $hash=$cap;
 
+date_default_timezone_set("America/Santiago");
+
+$time= date('Y-m-d H:i:s');
+
 include "conexion.php";
 
 
 
-  $consulta = "UPDATE `masgps`.`hash` SET `hash` = '$hash' WHERE (`id` = '4');";
+  
+  $qry="UPDATE `masgps`.`hash` SET `timestamp` = '$time', `hash` = '$hash' WHERE (`id` = '4');";
 
-  $resutaldo3 = mysqli_query($mysqli, $consulta);
+  $resutaldo3 = mysqli_query($mysqli, $qry);
 
 
 
