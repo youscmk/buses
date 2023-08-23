3e6e656e054ef2a8ddddab2c69408e778ff0b45a
Dorian <dorian.celu@gmail.com>
2023-08-23 16:13:42 -0400

    las-condes-lite

--- lascondesLite.php
+++ lascondesLite.php
@@ -19,7 +19,7 @@ $data=mysqli_fetch_array($resutaldo);
 
 $cap=$data['hash'];
 
-header("refresh:30");
+
 
 $listado = '';
 
@@ -52,6 +52,8 @@ $response2 = curl_exec($curl);
 $json = json_decode($response2);
 
 $array = $json->list;
+
+header("refresh:30");
 
 foreach ($array as $item) {
 
@@ -101,38 +103,8 @@ foreach ($array as $item) {
   //echo " , &nbsp";
   $status=$json2->state->connection_status;
 
-  $curl = curl_init();
-
-  curl_setopt_array($curl, array(
-    CURLOPT_URL => 'http://www.trackermasgps.com/api-v2/geocoder/search_location',
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => '',
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => 'POST',
-    CURLOPT_POSTFIELDS => '{"location":{"lat":' . $lat . ',"lng":' . $lng . '},"lang":"es","hash":"' . $cap . '"}',
-    CURLOPT_HTTPHEADER => array(
-      'Accept: application/json, text/plain, */*',
-      'Accept-Language: es-419,es;q=0.9,en;q=0.8',
-      'Connection: keep-alive',
-      'Content-Type: application/json',
-      'Cookie: _ga=GA1.2.728367267.1665672802; _gid=GA1.2.343013326.1670594107; locale=es; session_key=7ceae24d013dd694bdbaa06dd8bac781; check_audit=7ceae24d013dd694bdbaa06dd8bac781',
-      'Origin: http://www.trackermasgps.com',
-      'Referer: http://www.trackermasgps.com/',
-      'User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Mobile Safari/537.36'
-    ),
-  ));
-
-  $response = curl_exec($curl);
-
-  curl_close($curl);
-  $json1 = json_decode($response);
-  $array4 = $json1->value;
-  $direcc = $array4;
-
-  $direcc1 = addslashes($direcc);
+  
+  $direcc1 = "**";
 	//echo $direcc1;
   
   date_default_timezone_set("America/Santiago");
