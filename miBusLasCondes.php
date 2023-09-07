f0a63b8ad0402cb18f92d8f3f9a8431c45cd6a58
Dorian <dorian.celu@gmail.com>
2023-09-07 12:34:54 -0300

    Cors

--- miBusLasCondes.php
+++ miBusLasCondes.php
@@ -1,5 +1,7 @@
 <?php
-
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 
 include "login/conexion.php";
 
