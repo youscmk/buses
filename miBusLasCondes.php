a5c9b7de17b581c3c92d9bb98f7bb93e5d58a5c5
Dorian <dorian.celu@gmail.com>
2023-09-07 12:24:04 -0300

    miBusLasCondes

---
+++ miBusLasCondes.php
@@ -0,0 +1,40 @@
+<?php
+
+
+include "login/conexion.php";
+
+$user="dorian";
+
+$pasw="123";
+
+include "login/conexion.php";
+
+$consulta="SELECT hash FROM masgps.hash where user='$user' and pasw='$pasw'";
+
+$resutaldo= mysqli_query($mysqli,$consulta);
+
+$data=mysqli_fetch_array($resutaldo);
+
+$cap=$data['hash'];
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'http://www.trackermasgps.com/api-v2/tracker/list',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS =>'{"hash": "'.$cap.'"}',
+  CURLOPT_HTTPHEADER => array(
+    'Content-Type: application/json'
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
