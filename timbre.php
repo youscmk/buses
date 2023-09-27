a15353b9e52c4fdd130d69faf03f6dec2b602368
Dorian <dorian.celu@gmail.com>
2023-09-27 16:43:15 -0300

    timbre

---
+++ timbre.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Timbre</title>
+    <style>
+        *{
+            margin-top: 0px;
+            margin-bottom: 0px;
+            padding-left: 0%;
+            padding-right: 0%;
+        }
+        .container {
+            font-family: sans-serif;
+            max-width: 430px;
+            height: 932px;
+            position: relative;
+            margin: auto;
+            color: #CB9C00;
+
+        }
+        #logo {
+           
+            height: 62px;
+            width: 114.63px;
+           
+            position: relative;
+            margin-top: 149px;
+            margin-left: 130px;
+            
+
+        }
+        #help {
+          
+           
+            height: 54px;
+            width:190px ;
+            position: absolute;
+            margin-top: 214px;
+            margin-left: 98px;
+            text-align: center;
+        }
+        p#titulo {
+            
+            font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            font-size: 20px;
+            font-weight: bold;
+
+        }
+
+        p {
+            
+            font-size: 12px;
+        }
+
+        #telefono {
+            position: absolute;
+            margin-top:323px ;
+            margin-left: 137px;
+        }
+
+        #horario {
+           position: absolute;
+           width: 206px;
+           height: 54px; 
+           margin-top: 482px;
+           margin-left: 85px;
+           
+          
+           
+           
+
+        }
+        #horario p {
+            height: 50px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+
+<div class="container">
+
+   
+
+    <div id="logo">
+        <img  src="./img/logo.png" alt="sportlife">
+    </div>
+    <div id="help">
+
+    <img src="./img/Timbre.png" alt="">
+     
+
+    </div>
+    <div id="telefono">
+
+       <a href="tel:+56989526365"> <img src="./img/Bell.png" width="187px" alt=""></a>
+
+    </div>
+
+    <div id="horario">
+        <img src="./img/toque.png" alt="">
+        
+    </div>
+
+    
+</div>
+    
+</body>
+</html>
---
+++ timbre2.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Timbre</title>
+    <style>
+        *{
+            margin-top: 0px;
+            margin-bottom: 0px;
+            padding-left: 0%;
+            padding-right: 0%;
+        }
+        .container {
+            font-family: sans-serif;
+            max-width: 430px;
+            height: 932px;
+            position: relative;
+            margin: auto;
+            color: #CB9C00;
+
+        }
+        #logo {
+           
+            height: 150px;
+         
+            text-align: center;
+            
+     
+            
+
+        }
+        #help {
+          
+           
+            height: 100px;
+           
+            text-align: center;
+        }
+
+    
+
+        #telefono {
+          
+            height: 260px;
+        
+            text-align: center;
+     ;
+        }
+
+        #horario {
+ 
+            height: 50px;
+          
+            text-align: center;
+          
+           
+           
+
+        }
+        #horario p {
+            height: 50px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+
+<div class="container">
+
+   
+
+    <div id="logo">
+        <img  src="./img/logo.png" height="60" alt="sportlife" style="margin-top: 90px;">
+    </div>
+    <div id="help">
+
+    <img src="./img/Timbre.png" alt="" height="40px" style="margin-top: 60px;">
+     
+
+    </div>
+    <div id="telefono">
+
+       <a href="tel:+56989526365"> <img src="./img/Bell.png" width="187px" alt="" style="margin-top:70px ;"></a>
+
+    </div>
+
+    <div id="horario">
+        <img src="./img/toque.png" alt="">
+        
+    </div>
+
+    
+</div>
+    
+</body>
+</html>
