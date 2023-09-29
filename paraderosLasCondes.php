ddbb55b8c5a7c088065586212c3a5acd36988ec9
Dorian <dorian.celu@gmail.com>
2023-09-29 13:01:03 -0300

    Paraderos from bbdd

---
+++ paraderosLasCondes.php
@@ -0,0 +1,40 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+
+include "login/conexion.php";
+
+$consulta="SELECT patente, paradero, direccion, latitud, longitud FROM masgps.paraderos;";
+
+$resutaldo= mysqli_query($mysqli,$consulta);
+$i=0;
+
+while($data=mysqli_fetch_array($resutaldo)){
+
+   $dir=substr( $data['direccion'] , 0,35);
+   
+
+     $json =[
+       'paradero'=> $data['paradero'],
+       'patente'=>$data['patente'],
+       'direccion'=> $dir ,
+       'coord'=>[
+       'lat'=>$data['latitud'],'lng'=>$data['longitud']
+       ]
+    
+    
+    ];
+
+     $total[$i]=$json;
+
+     $i++;
+};
+
+echo json_encode($total, http_response_code(200));
+
+
+
+
+
