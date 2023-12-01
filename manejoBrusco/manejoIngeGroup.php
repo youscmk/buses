da4944b7388a6ab89e214022a6fb7a6869befcb5
Dorian <dorian.celu@gmail.com>
2023-12-01 15:39:37 -0300

    manejo brusco

---
+++ manejoBrusco/manejoIngeGroup.php
@@ -0,0 +1,20 @@
+<?php 
+
+date_default_timezone_set("America/Santiago");
+
+$hoy = date("Y-m-d");
+
+$ayer=date('Y-m-d',strtotime("-1 days"));
+
+$user="Ingegroup";
+
+$pasw="123";
+
+include "manejoBrusco.php";
+
+manejo($user,$pasw,$ayer);
+
+
+
+
+?>
